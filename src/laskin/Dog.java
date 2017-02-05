be257478b9c0abdcf2627d84ba5fbecd15a6837c
Tam Tran <tam.tran1080@gmail.com>
2017-02-05 13:53:42 +0200

    add existing file

---
+++ src/laskin/Dog.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package laskin;
+
+/**
+ *
+ * @author Tam Tran
+ */
+public class Dog {
+String breed;
+int ageC;
+String color;
+
+   void barking() {
+   }
+
+   void hungry() {
+   }
+
+   void sleeping() {
+   }
+}
